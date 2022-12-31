a02af53a1f5a798b4bd58e3a53938679bae4681e
yoonbeomjun <65801966+yoonbeomjun@users.noreply.github.com>
2023-01-01 00:12:06 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const port = 3000;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('hello world!');
 });
 
-module.exports = router;
+app.listen(port, function (err) {
+  console.log('Connected port - ' + port);
+  if (err) {
+    return console.log('Found error - ', err);
+  }
+});
